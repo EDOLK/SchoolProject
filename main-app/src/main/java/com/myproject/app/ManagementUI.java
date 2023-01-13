409711690132c870035f553e8c3f3f6ea87c201a
EDOLK <119468544+EDOLK@users.noreply.github.com>
2023-01-13 18:26:09 -0500

    gitignore + cleanup

--- main-app/src/main/java/com/myproject/app/ManagementUI.java
+++ main-app/src/main/java/com/myproject/app/ManagementUI.java
@@ -1,9 +1,15 @@
 package com.myproject.app;
 
-public class ManagementUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ManagementUI extends Application{
     public static void main(String[] args) {
-        
+        launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception{
+    }
     
 }
